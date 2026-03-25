236b9060fdcb242f04117f798625350fc4c60a9e
KALDYBAKYZY <zhaiylgangulnaz@gmail.com>
2026-03-25 19:52:40 +0500

    second changes

--- handlers/author_handler.go
+++ handlers/author_handler.go
@@ -10,24 +10,19 @@ import (
 var authors = []models.Author{}
 var nextAuthorID = 1
 
-func GetAuthors(w http.ResponseWriter, r *http.Request) {
+func GetAuthors(w http.ResponseWriter, _ *http.Request) {
 	json.NewEncoder(w).Encode(authors)
 }
 
 func CreateAuthor(w http.ResponseWriter, r *http.Request) {
-
 	var author models.Author
 	json.NewDecoder(r.Body).Decode(&author)
-
 	if author.Name == "" {
 		http.Error(w, "Name required", http.StatusBadRequest)
 		return
 	}
-
 	author.ID = nextAuthorID
 	nextAuthorID++
-
 	authors = append(authors, author)
-
 	json.NewEncoder(w).Encode(author)
 }
--- handlers/category_handler.go
+++ handlers/category_handler.go
@@ -10,24 +10,19 @@ import (
 var categories = []models.Category{}
 var nextCategoryID = 1
 
-func GetCategories(w http.ResponseWriter, r *http.Request) {
+func GetCategories(w http.ResponseWriter, _ *http.Request) {
 	json.NewEncoder(w).Encode(categories)
 }
 
 func CreateCategory(w http.ResponseWriter, r *http.Request) {
-
 	var category models.Category
 	json.NewDecoder(r.Body).Decode(&category)
-
 	if category.Name == "" {
 		http.Error(w, "Name required", http.StatusBadRequest)
 		return
 	}
-
 	category.ID = nextCategoryID
 	nextCategoryID++
-
 	categories = append(categories, category)
-
 	json.NewEncoder(w).Encode(category)
 }
